2da19cb895a97fcf0572a85d089452887e0a58bc
Swapnil Singh <swapnilskumars@gmail.com>
2023-12-20 18:57:40 +0530

    done till login page

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  <>
+    <h2 class="">Login with Google</h2>;
+  </>;
 };
 
 export default Dashboard;
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div class="container mx-auto p-8 bg-white rounded shadow-md flex justify-center flex-col items-center w-fit-content">
+        <h2 class="text-2xl font-bold mb-4">Login with Google</h2>
+        <button
+          id="loginButton"
+          class="bg-blue-500 text-white py-2 px-4 rounded"
+        >
+          Login with Google
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Login;
